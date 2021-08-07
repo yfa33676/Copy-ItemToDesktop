7d9017920465130641d8751e79b30a5ac9e3e29f
yfa33676 <yfa33676@gmail.com>
2021-08-07 11:48:40 +0900

    初回

---
+++ Copy-ItemToDesktop.psm1
@@ -0,0 +1,29 @@
+function Copy-ItemToDesktop{
+     Param(
+       [Parameter(ValueFromPipeline)]
+       [psobject]$InputObject,
+       [Parameter(ValueFromPipeline)]
+       [string[]]$Path
+     )
+     Process{
+       if($Path -ne ""){
+         $InputObject = Get-Item -Path $Path
+       }
+       if($InputObject.PSIsContainer){
+         $Destination = "~\Desktop" + ($InputObject.FullName | Split-Path -NoQualifier)
+         if(Test-Path $Destination){
+             Copy-Item -Recurse -Force -Path $InputObject.FullName -Destination ($Destination | Split-Path)
+         } else {
+             Copy-Item -Recurse -Path $InputObject.FullName -Destination $Destination
+         }
+       } else{
+         $Destination = "~\Desktop" + ($InputObject.FullName | Split-Path -NoQualifier)
+         if(Test-Path ($Destination | Split-Path)){
+             Copy-Item -Path $InputObject.FullName -Destination $Destination
+         } else {
+             Copy-Item -Path ($InputObject.FullName | Split-Path) -Destination ($Destination | Split-Path)
+             Copy-Item -Path $InputObject.FullName -Destination $Destination
+         }
+       }
+     }
+}
